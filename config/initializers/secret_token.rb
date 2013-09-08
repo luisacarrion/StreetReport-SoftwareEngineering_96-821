8f4eedae64c61dbf04dac99b9de05159f38edda7
Maria Luisa Carrion D <marilu597@gmail.com>
2013-09-07 21:35:57 -0400

    Create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreetReport02::Application.config.secret_token = '0c6452c2a6ec09c78b24d8c8c39c99a89ddfa5af4a75ff1d21b13c2968f6e904687bc5fe081dd0d5cfbde4b745e873e1305a0fa9a9f055e1c7b775f803dbe0dd'
